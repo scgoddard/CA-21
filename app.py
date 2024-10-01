83366e26ac82856a3b4ea0f19a4d7526510baa5a
scgoddard <145470609+scgoddard@users.noreply.github.com>
2024-10-01 11:45:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.write(f"Data for the year {selected_year}:")
 filtered_df = df[df['Year'] = selected_year]
 st.write(filtered_df)
 
-fig, ax =plt.subplots()
+fig, ax = plt.subplots()
 ax.bar(filtered_df['Region'], filtered_df['Sales Amount'], color='skyblue')
 plt.title(f'Sales in {selected_year}')
 plt.xlabel('Region')
